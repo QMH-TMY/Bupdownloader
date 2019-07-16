7b9e9500d0c3f1ad3e8c02202af194d6e36d2a51
shieber <QMH_XB_FLTMY@yahoo.com>
2019-07-16 10:50:34 +0800

    change assert

--- BupDownloader.py
+++ BupDownloader.py
@@ -216,11 +216,10 @@ class AvSpider():
 
         #第一页的第一个视频模式不同，单独处理
         video_new = video_ul.find_all('li',class_='list-item clearfix fakeDanmu-item new')
-        #if video_new != []:
-        #    for new in video_new:
-        #        video_avs.append(new['data-aid'])
+        if video_new != []:
+            for new in video_new:
+                video_avs.append(new['data-aid'])
 
-        assert len(video_new) != 0
         for new in video_new:
             video_avs.append(new['data-aid'])
 
@@ -233,9 +232,8 @@ class AvSpider():
 
 def download_multi(video_avs):
     '''多进程下载'''
-    #if video_avs == []:
-    #    return None
-    assert len(video_avs) != 0
+    if video_avs == []:
+        return None
 
     pool = Pool(5)
     for av in video_avs:
